a0deb2b11bb96a2b467c92f02d8cad0e204f07c4
Joy Roy <joyroy261216@gmail.com>
2021-07-02 01:07:23 +0600

    Form Project Start

--- src/components/Composition.jsx
+++
@@ -1,8 +0,0 @@
-import Emoji from './composition/Emoji.jsx'
-export default function Composition() {
-    return (<section class="container">
-        <h3 class="text-center">Composition in React</h3>
-        <Emoji />
-        <hr />
-    </section>)
-}
